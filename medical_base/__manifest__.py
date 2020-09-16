c2da36a385f38f39cdf15c5770f2ca0613ea5474
Luis Rodríguez <luisrs8608@gmail.com>
2020-09-16 00:55:00 -0300

    Adding medical base addon

---
+++ medical_base/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+
+{
+    'name': 'Medical',
+    'version': '1.0',
+    'author': "Luis Rodriguez",
+    # 'website': 'http://www.cloudlt.com',
+    'category': 'Hidden',
+    'depends': ['l10n_latam_base', 'l10n_uy'],
+    'description': """
+    """,
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/res_partner_views.xml',
+        # 'views/sale_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
+
