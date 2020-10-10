5c2373a9d6f08c4199c0390140b066563c4ae9ef
Luis Rodríguez <luisrs8608@gmail.com>
2020-10-10 00:41:59 -0300

    adding appointments

--- medical_base/models/appointment.py
+++ medical_base/models/appointment.py
@@ -7,10 +7,12 @@ from odoo import api, fields, models, _
 class Appointment(models.Model):
     _description = 'Appointment'
     _name = 'appointment'
-    _rec_name='date'
-    _order = 'date DESC'
+    _rec_name = 'patient_id'
+    _order = 'start DESC'
 
     patient_id = fields.Many2one('res.partner', string='Patient', index=True, ondelete='cascade')
-    date = fields.Date(string='Date', default=fields.Date.today)
-    date_stop = fields.Date(string='Date stop', default=fields.Date.today)
+    state = fields.Selection([('draft', 'Unconfirmed'), ('open', 'Confirmed')], string='Status', readonly=True, tracking=True, default='draft')
+    start = fields.Datetime('Start', required=True)
+    stop = fields.Datetime('Stop', required=True)
+    duration = fields.Float('Duration', states={'done': [('readonly', True)]})
     employee_id = fields.Many2one('hr.employee', string='Dr.(Dra)', index=True, ondelete='cascade')
