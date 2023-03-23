e91c21780566b99a816084fc1e5618c1e6c2f3a9
Luis Rodriguez <luisrs8608@gmail.com>
2023-03-22 22:01:26 -0300

    Updating patient model and appointment

--- addons/medical_base/models/appointment.py
+++ addons/medical_base/models/appointment.py
@@ -9,13 +9,19 @@ class Appointment(models.Model):
     _rec_name = 'patient_id'
     # _order = 'start DESC'
 
-    patient_id = fields.Many2one('res.partner', string='Patient', index=True, ondelete='cascade')
+    patient_id = fields.Many2one(
+        'res.partner',
+        string='Patient',
+        domain=[('is_patient', '=', True)],
+        index=True,
+        ondelete='cascade'
+    )
     employee_id = fields.Many2one('hr.employee', string='Dr.(Dra)', index=True, ondelete='cascade')
 
     @api.onchange('patient_id')
     def _onchange_patient_id(self):
         for rec in self:
-            rec.name = rec.patient_id
+            rec.name = rec.patient_id.name
 
     @api.onchange('name')
     def _onchange_name(self):
@@ -23,3 +29,11 @@ class Appointment(models.Model):
             if not rec.patient_id:
                 partner_id = self.env['res.partner'].search([('name', 'ilike', rec.name)], limit=1)
                 rec.patient_id = partner_id and partner_id.id or False
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        vals_list = [
+            dict(vals, allday=False) for vals in vals_list
+        ]
+        res_ids = super(Appointment, self).create(vals_list)
+        return res_ids
--- addons/medical_base/models/partner.py
+++ addons/medical_base/models/partner.py
@@ -10,6 +10,7 @@ class Patient(models.Model):
     clinic_history_ids = fields.One2many('clinic.history', 'patient_id', string='Clinic history')
     employee_id = fields.Many2one('hr.employee', string='Dr.(Dra)', index=True, ondelete='cascade')
     calendar_event_ids = fields.One2many('calendar.event', 'patient_id', string='Medical appointments')
+    is_patient = fields.Boolean('Is patient?')
 
     def new_treatment(self):
         values = []
